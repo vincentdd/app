feb914c5f68cf3f45ac3b490f06611c00cb5f678
vincentdd <scar_dante@hotmail.com>
2019-05-04 21:38:00 +0800

    no message

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,21 +4,27 @@ const { sanitizeBody } = require('express-validator/filter');
 const UserService = require('../services/UserService');
 
 exports.sign_up = [
-    body('user_name').isLength({ min: 1 }).trim().withMessage('user name must be specified.')
+    body('username').isLength({ min: 1 }).trim().withMessage('user name must be specified.')
         .isAlphanumeric().withMessage('user name has non-alphanumeric characters.'),
     body('password').isLength({ min: 1 }).trim().withMessage('password must be specified.')
         .isAlphanumeric().withMessage('password has non-alphanumeric characters.'),
-    sanitizeBody('user_name').trim().escape(),
+    sanitizeBody('username').trim().escape(),
     sanitizeBody('password').trim().escape(),
     (req, res, next) => {
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
             // There are errors. Render form again with sanitized values/errors messages.
             //res.render('user_form', { title: 'Create User', author: req.body, errors: errors.array() });
-            res.json({errors: errors.mapped()});
+            console.log(errors.mapped());
+            const responseData = {
+                ...res.locals.responseData,
+                code: -1,
+                message: errors.mapped().username.msg
+            };
+            res.json(responseData);
             return;
         }else {
-            User.find({user_name:  req.body.user_name})
+            User.find({username:  req.body.username})
                 .then(function (_user) {
                     if(_user.length !== 0) {
                         res.send("Username is already taken");
@@ -26,7 +32,7 @@ exports.sign_up = [
                         return;
                     }else{
                         const user = new User({
-                            user_name: req.body.user_name,
+                            username: req.body.username,
                             password: req.body.password
                         });
                         const userService = new UserService();
